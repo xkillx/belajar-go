27143ef7bf17643d22991373adff00d398eb0507
Azhar Khalid <azhar.tkjzone@gmail.com>
2018-05-25 23:11:04 +0700

    Add Simple HTTP Server With Mux and Template

---
+++ SimpleHTTPServer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"html/template"
+)
+
+var templates *template.Template
+
+func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
